c5acd49fb37bf7f4b5843efa3177c195ee3a1161
danlindleybaker <danlindleybaker@gmail.com>
2022-06-13 14:35:29 +0100

    cleaning up and refactoring

--- vft_plot.jl
+++ vft_plot.jl
@@ -10,11 +10,12 @@ param,sigma = vft_fit(tau_alpha[:,1],tau_alpha[:,2])
 y_fit = vft(tau_alpha[:,1],param)
 y_fit = [10^x for x in y_fit]
 
-fig,ax = subplots()
 close("all")
+fig,ax = subplots()
 ax.semilogy([1000/x for x in tau_alpha[:,1]],tau_alpha[:,2],"ro")
 ax.semilogy([1000/x for x in tau_alpha[:,1]],y_fit,"k-")
-#savefig("test")
+
+savefig("matt")
 
 display(gcf())
 println("works")
